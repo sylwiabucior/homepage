c2dcb2cc8118d3aee36ca56ad0b6b2b0b3062363
Sylwia Bucior <sylwiamarkiewicz.1@gmail.com>
2023-07-24 22:22:59 +0200

    dodanie klas

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ alert('Siemanko JS');
 console.log(firstName);
 console.log(age);
 console.log(`nazywam się ${firstName} i mam ${age} lata`);
+
+const header= document.querySelector ('h1')
+console.log(header);
+
+header.innerHTML= 'Właśnie podmieniłam tekst na stronie za pomocą JS'
