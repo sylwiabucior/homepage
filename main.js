1d6b2c549919d2230fe582a00d39598cddbaaf2b
Sylwia Bucior <sylwiamarkiewicz.1@gmail.com>
2023-08-06 11:52:41 +0200

    homework week nr 4

--- main.js
+++ main.js
@@ -11,3 +11,6 @@ const header= document.querySelector ('h1')
 console.log(header);
 
 header.innerHTML= 'Właśnie podmieniłam tekst na stronie za pomocą JS'
+
+const contentJs= document.querySelector (".section__text--js")
+contentJs.innerHTML= 'Dzień dobry! Właśnie wstawiłam tekst JS jako praca domowa. Trochę z opóźnieniem ale się udało'
